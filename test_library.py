37d23da89f83e93eec9be3b329c99f01c753d7dc
Viraagjain <viraagborundiya72@gmail.com>
2024-09-26 22:34:15 +0530

    Add test for adding books

---
+++ test_library.py
@@ -0,0 +1,9 @@
+import pytest
+from library import Library, Book
+
+def test_add_book():
+    library = Library()
+    book = Book(isbn="1234567890", title="Python 101", author="John Doe", year=2020)
+    library.add_book(book)
+    assert len(library.books) == 1
+    assert library.books[0].title == "Python 101"
