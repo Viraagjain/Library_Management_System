6779cb5a86b9e84787b8b91beb272a1e348c4d81
Viraagjain <viraagborundiya72@gmail.com>
2024-09-26 22:36:12 +0530

    Add test for borrowing books

--- test_library.py
+++ test_library.py
@@ -7,3 +7,11 @@ def test_add_book():
     library.add_book(book)
     assert len(library.books) == 1
     assert library.books[0].title == "Python 101"
+
+def test_borrow_book():
+    library = Library()
+    book = Book(isbn="1234567890", title="Python 101", author="John Doe", year=2020)
+    library.add_book(book)
+    library.borrow_book("1234567890")
+    assert not library.books[0].available
+
